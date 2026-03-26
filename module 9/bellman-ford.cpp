d9789021d568b9f37320d87ff936ea2e98ae3e15
TashdidDiganta <tashdiddiganta@gmail.com>
2026-03-26 22:09:53 +0600

    Module 9

---
+++ module 9/bellman-ford.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int dis[1005];
+vector<Edge> edge_list;
+int n, e;
+class Edge
+{
+public:
+   int a, b, c;
+   Edge(int a, int b, int c)
+   {
+      this->a = a;
+      this->b = b;
+      this->c = c;
+   }
+};
+
+void bellman_ford()
+{
+   for (int i = 0; i < n - 1; i++)
+   {
+      for (auto ed : edge_list)
+      {
+         int a, b, c;
+         a = ed.a;
+         b = ed.b;
+         c = ed.c;
+         if (dis[a] != INT_MAX && dis[a] + c < dis[b])
+         {
+            dis[b] = dis[a] + c;
+         }
+      }
+   }
+
+   bool cycle = false;
+
+   for (auto ed : edge_list)
+   {
+      int a, b, c;
+      a = ed.a;
+      b = ed.b;
+      c = ed.c;
+      if (dis[a] != INT_MAX && dis[a] + c < dis[b])
+      {
+         cycle = true;
+         break;
+      }
+   }
+   if (cycle)
+   {
+      cout << "Negetive cycle detected\n";
+   }
+   else
+   {
+      cout << "NO Negetive cycle detected\n";
+   }
+}
+
+int main()
+{
+
+   cin >> n >> e;
+
+   while (e--)
+   {
+      int a, b, c;
+      cin >> a >> b >> c;
+      edge_list.push_back(Edge(a, b, c));
+   }
+
+   for (int i = 0; i < n; i++)
+   {
+      dis[i] = INT_MAX;
+   }
+   dis[0] = 0;
+
+   bellman_ford();
+
+   for (int i = 0; i < n; i++)
+   {
+      cout << i << " -> " << dis[i] << endl;
+   }
+
+   return 0;
+}
